7603276ed8a2795a564107128f6504e7864b6d98
surenayrapetyan-create <suren.ayrapetyan@gmail.com>
2026-04-23 15:22:48 +0300

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const { prompt } = req.body;
 
   try {
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GOOGLE_API_KEY}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash-001:generateContent?key=${process.env.GOOGLE_API_KEY}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
